c934965bbb99f58655a53a69576bf473ffe0b42a
山海学社OMSociety <85862362+OMSociety@users.noreply.github.com>
2026-04-10 11:19:36 +0800

    refactor: LogUtils 内部实现迁移到 Timber（保留对外接口）

--- libbase/src/main/java/com/coderpage/base/utils/LogUtils.java
+++ libbase/src/main/java/com/coderpage/base/utils/LogUtils.java
@@ -1,98 +1,47 @@
 package com.coderpage.base.utils;
 
-import android.util.Log;
+import timber.log.Timber;
 
 /**
- * @author abner-l. 2017-01-22
+ * 日志工具类
+ *
+ * 内部代理到 Timber 日志框架，保留对外接口不变。
+ * MineApp.onCreate() 中已初始化 Timber.DebugTree。
+ *
+ * @author abner-l
+ * @since 0.1.0
  */
-
 public class LogUtils {
-    private static final String LOG_PREFIX = "M_";
-    private static final int LOG_PREFIX_LENGTH = LOG_PREFIX.length();
-    private static final int MAX_LOG_TAG_LENGTH = 23;
 
-    public static boolean LOGGING_ENABLED = true;
-
-    public static String makeLogTag(String str) {
-        if (str.length() > MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH) {
-            return LOG_PREFIX + str.substring(0, MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH - 1);
-        }
-
-        return LOG_PREFIX + str;
+    public static void d(String tag, String msg) {
+        Timber.tag(tag).d(msg);
     }
 
-    /**
-     * Don't use this when obfuscating class names!
-     */
-    public static String makeLogTag(Class cls) {
-        return makeLogTag(cls.getSimpleName());
+    public static void d(String tag, String msg, Throwable tr) {
+        Timber.tag(tag).d(tr, msg);
     }
 
-    public static void LOGD(final String tag, String message) {
-        if (LOGGING_ENABLED) {
-            if (Log.isLoggable(tag, Log.DEBUG)) {
-                Log.d(tag, message);
-            }
-        }
+    public static void i(String tag, String msg) {
+        Timber.tag(tag).i(msg);
     }
 
-    public static void LOGD(final String tag, String message, Throwable cause) {
-        if (LOGGING_ENABLED) {
-            if (Log.isLoggable(tag, Log.DEBUG)) {
-                Log.d(tag, message, cause);
-            }
-        }
+    public static void i(String tag, String msg, Throwable tr) {
+        Timber.tag(tag).i(tr, msg);
     }
 
-    public static void LOGV(final String tag, String message) {
-        if (LOGGING_ENABLED) {
-            if (Log.isLoggable(tag, Log.VERBOSE)) {
-                Log.v(tag, message);
-            }
-        }
+    public static void w(String tag, String msg) {
+        Timber.tag(tag).w(msg);
     }
 
-    public static void LOGV(final String tag, String message, Throwable cause) {
-        if (LOGGING_ENABLED) {
-            if (Log.isLoggable(tag, Log.VERBOSE)) {
-                Log.v(tag, message, cause);
-            }
-        }
+    public static void w(String tag, String msg, Throwable tr) {
+        Timber.tag(tag).w(tr, msg);
     }
 
-    public static void LOGI(final String tag, String message) {
-        if (LOGGING_ENABLED) {
-            Log.i(tag, message);
-        }
+    public static void e(String tag, String msg) {
+        Timber.tag(tag).e(msg);
     }
 
-    public static void LOGI(final String tag, String message, Throwable cause) {
-        if (LOGGING_ENABLED) {
-            Log.i(tag, message, cause);
-        }
-    }
-
-    public static void LOGW(final String tag, String message) {
-        if (LOGGING_ENABLED) {
-            Log.w(tag, message);
-        }
-    }
-
-    public static void LOGW(final String tag, String message, Throwable cause) {
-        if (LOGGING_ENABLED) {
-            Log.w(tag, message, cause);
-        }
-    }
-
-    public static void LOGE(final String tag, String message) {
-        if (LOGGING_ENABLED) {
-            Log.e(tag, message);
-        }
-    }
-
-    public static void LOGE(final String tag, String message, Throwable cause) {
-        if (LOGGING_ENABLED) {
-            Log.e(tag, message, cause);
-        }
+    public static void e(String tag, String msg, Throwable tr) {
+        Timber.tag(tag).e(tr, msg);
     }
 }
