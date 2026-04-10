1026aaf2c639a8e0d9650ab4e768c1e994fbec85
山海学社OMSociety <85862362+OMSociety@users.noreply.github.com>
2026-04-10 10:09:30 +0800

    fix: IC_NAME_GOU_WU 注释从'住房'改为'购物'

--- app/src/main/java/com/coderpage/mine/app/tally/data/CategoryIconHelper.java
+++ app/src/main/java/com/coderpage/mine/app/tally/data/CategoryIconHelper.java
@@ -19,7 +19,7 @@ public class CategoryIconHelper {
     public static final String IC_NAME_CAN_YIN = "CanYin";
     /** 交通 */
     public static final String IC_NAME_JIAO_TONG = "JiaoTong";
-    /** 住房 */
+    /** 购物 */
     public static final String IC_NAME_GOU_WU = "GouWu";
     /** 服饰 */
     public static final String IC_NAME_FU_SHI = "FuShi";
