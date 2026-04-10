bdf25120fcd67c858b8ce9643f5c8b534b4207e6
山海学社OMSociety <85862362+OMSociety@users.noreply.github.com>
2026-04-10 17:20:39 +0800

    fix: 迁移 android.arch 到 AndroidX (MineDatabase)

--- app/src/main/java/com/coderpage/mine/persistence/database/MineDatabase.java
+++ app/src/main/java/com/coderpage/mine/persistence/database/MineDatabase.java
@@ -1,8 +1,8 @@
 package com.coderpage.mine.persistence.database;
 
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 import com.coderpage.mine.MineApp;
 import com.coderpage.mine.app.tally.persistence.sql.TallyDatabase;
