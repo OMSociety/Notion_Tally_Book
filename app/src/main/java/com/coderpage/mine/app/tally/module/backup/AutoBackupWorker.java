226d7740201c52fa3c0d1798e860867d12cf8238
山海学社OMSociety <85862362+OMSociety@users.noreply.github.com>
2026-04-10 10:14:09 +0800

    refactor: AutoBackupWorker.java 迁移到 AndroidX

--- app/src/main/java/com/coderpage/mine/app/tally/module/backup/AutoBackupWorker.java
+++ app/src/main/java/com/coderpage/mine/app/tally/module/backup/AutoBackupWorker.java
@@ -1,7 +1,7 @@
 package com.coderpage.mine.app.tally.module.backup;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 
 import androidx.work.Worker;
