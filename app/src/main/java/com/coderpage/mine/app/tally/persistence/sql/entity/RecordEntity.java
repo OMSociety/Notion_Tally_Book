532436bc1b76e02881dec2ec6a69db8894db9b4b
山海学社OMSociety <85862362+OMSociety@users.noreply.github.com>
2026-04-10 10:13:21 +0800

    refactor: RecordEntity.java 迁移旧包名到 AndroidX (android.arch→androidx.room, android.support→androidx.annotation)

--- app/src/main/java/com/coderpage/mine/app/tally/persistence/sql/entity/RecordEntity.java
+++ app/src/main/java/com/coderpage/mine/app/tally/persistence/sql/entity/RecordEntity.java
@@ -1,11 +1,11 @@
 package com.coderpage.mine.app.tally.persistence.sql.entity;
 
-import android.arch.persistence.room.ColumnInfo;
-import android.arch.persistence.room.Entity;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
 import com.coderpage.mine.app.tally.persistence.sql.DatabaseConstants;
-import android.arch.persistence.room.Index;
-import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+import androidx.annotation.NonNull;
 
 /**
  * @author lc.
